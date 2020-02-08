34d21c3bc90d28d88bb17294dfef994f562ec68b
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-02-08 18:09:43 +0800

    使用nacos 动态配置中心，实时刷新配置

--- hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/grady/apache/dubbo/provider/service/impl/EchoServiceImpl.java
+++ hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/grady/apache/dubbo/provider/service/impl/EchoServiceImpl.java
@@ -3,15 +3,20 @@ package com.grady.apache.dubbo.provider.service.impl;
 import com.grady.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @Service(version = "1.0.0")
 public class EchoServiceImpl implements EchoService {
 
     @Value("${dubbo.protocol.port}")
     private String port;
 
+    @Value("${diy.username}")
+    private String username;
+
     @Override
     public String echo(String str) {
-        return "rpc call: echo : " + str + " my port: " + port;
+        return "rpc call: echo : " + str + " my port: " + port + " diy.username" + username;
     }
 }
