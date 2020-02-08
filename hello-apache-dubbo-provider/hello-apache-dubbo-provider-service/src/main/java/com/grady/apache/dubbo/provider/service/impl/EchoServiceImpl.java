dfdfe3410e7d5e75e1c086c15badbcca0e6cb180
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-02-08 17:40:31 +0800

    服务提供者 负载均衡: 轮询

--- hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/grady/apache/dubbo/provider/service/impl/EchoServiceImpl.java
+++ hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/grady/apache/dubbo/provider/service/impl/EchoServiceImpl.java
@@ -2,12 +2,16 @@ package com.grady.apache.dubbo.provider.service.impl;
 
 import com.grady.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service(version = "1.0.0")
 public class EchoServiceImpl implements EchoService {
 
+    @Value("${dubbo.protocol.port}")
+    private String port;
+
     @Override
     public String echo(String str) {
-        return "rpc call: echo : " + str;
+        return "rpc call: echo : " + str + " my port: " + port;
     }
 }
